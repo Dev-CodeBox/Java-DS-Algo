6f53edd1f4298846c16a9d4f9d4ffcc359b245d8
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2024-12-08 11:59:58 +0000

    Reverse Array ( Two Pointer ) - LS

---
+++ DS-Algo/Array And ArrayList/src/Linear-Search/Reverse_Array.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Reverse_Array {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Static Array
+        int[] arr = new int[5];
+
+        // Inserting Elements Into arr
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter The Element For " + i + "th Index");
+            arr[i] = scanner.nextInt();
+        }
+
+        // Printing Elements Of arr
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+        reverseArray(arr);
+
+        // Printing Reversed Array
+        System.out.print("Reversed Array -> ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    // Reverse Array
+    public static void reverseArray(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
