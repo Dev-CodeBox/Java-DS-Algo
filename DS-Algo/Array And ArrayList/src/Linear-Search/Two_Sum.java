c7355f47e5cd1b83f679c12457aca2b4d78f4945
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2024-12-09 09:15:09 +0000

    Two Sum - LS

---
+++ DS-Algo/Array And ArrayList/src/Linear-Search/Two_Sum.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Two_Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Static Array
+        int[] arr = new int[5];
+
+        // Inserting Elements Into arr
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter The Element For " + i + "th Index");
+            arr[i] = scanner.nextInt();
+        }
+
+        // Printing Elements Of arr
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+        twoSum(arr);
+    }
+
+    // Two Sum
+    public static void twoSum(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j) {
+                    System.out.print(arr[i] + arr[j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
