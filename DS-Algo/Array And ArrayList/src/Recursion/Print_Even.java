ecf02b8858ba23c778451ca4b344f0b5ce35011e
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2024-12-26 08:44:49 +0000

    Print Even - Recursion

---
+++ DS-Algo/Array And ArrayList/src/Recursion/Print_Even.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Print_Even {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        System.out.print("Enter The Size For arrList : ");
+        int size = scanner.nextInt();
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter The Element For " + i + "th Index : ");
+            arrList.add(i, scanner.nextInt());
+        }
+        for (int i = 0; i < arrList.size(); i++) {
+            System.out.print(arrList.get(i) + " ");
+        }
+        System.out.println();
+        System.out.print("Evens In arrList -> ");
+        printEven(arrList, 0);
+    }
+
+    // Print Odd
+    public static void printEven(ArrayList<Integer> arrList, int index) {
+        if (index == arrList.size()) {
+            return;
+        }
+        if (arrList.get(index) % 2 == 0) {
+            System.out.print(arrList.get(index) + " ");
+        }
+        printEven(arrList, index + 1);
+    }
+}
