6bf4fea6c2a3ef398a4231a8b8237a0d86d9ea0f
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-02-01 08:16:21 +0000

    Insertion At Head

--- DS-Algo/Linked List/src/CustomLinkedList.java
+++ DS-Algo/Linked List/src/CustomLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CustomLinkedList {
     class Node {
         int data;
@@ -18,6 +20,20 @@ public class CustomLinkedList {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         CustomLinkedList list = new CustomLinkedList();
+        list.insertAtHead(scanner.nextInt());
+    }
+
+    public void insertAtHead(int data) {
+        if (head == null && tail == null) {
+            Node node = new Node(data);
+            head = node;
+            tail = node;
+        } else {
+            Node node = new Node(data);
+            node.next = head;
+            head = node;
+        }
     }
 }
