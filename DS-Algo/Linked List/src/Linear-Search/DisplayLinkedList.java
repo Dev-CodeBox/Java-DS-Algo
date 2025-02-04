ab2716cd7c97ab821ec979b328544efac127ca1b
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-02-04 09:30:48 +0000

    Display Linked List

---
+++ DS-Algo/Linked List/src/Linear-Search/DisplayLinkedList.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DisplayLinkedList {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+    Node tail;
+
+    DisplayLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DisplayLinkedList list = new DisplayLinkedList();
+        list.insertAtHead(scanner.nextInt());
+        list.insertAtHead(scanner.nextInt());
+        list.insertAtHead(scanner.nextInt());
+        list.insertAtHead(scanner.nextInt());
+        list.insertAtHead(scanner.nextInt());
+        System.out.println(list.displayList());
+
+    }
+
+    public ArrayList<Integer> displayList() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Node temp = head;
+        while (temp != null) {
+            list.add(temp.data);
+            temp = temp.next;
+        }
+        return list;
+    }
+
+    public void insertAtHead(int data) {
+        if (head == null && tail == null) {
+            Node node = new Node(data);
+            head = node;
+            tail = node;
+        } else {
+            Node node = new Node(data);
+            node.next = head;
+            head = node;
+        }
+    }
+
+    public void insertAtTail(int data) {
+        if (head == null && tail == null) {
+            Node node = new Node(data);
+            head = node;
+            tail = node;
+        } else {
+            Node node = new Node(data);
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+}
