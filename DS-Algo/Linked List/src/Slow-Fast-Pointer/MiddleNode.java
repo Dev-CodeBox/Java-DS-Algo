0e35e6cf682909c688fe83ed3044f2bdff603d35
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-02-10 09:29:41 +0000

    Middle Node ( Turtle And Hare )

---
+++ DS-Algo/Linked List/src/Slow-Fast-Pointer/MiddleNode.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class MiddleNode {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+    Node tail;
+
+    MiddleNode() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MiddleNode list = new MiddleNode();
+        list.insertAtHead(scanner.nextInt());
+        list.insertAtHead(scanner.nextInt());
+        list.insertAtHead(scanner.nextInt());
+        list.insertAtHead(scanner.nextInt());
+        list.insertAtHead(scanner.nextInt());
+        list.printList();
+        System.out.println("Middle Node -> " + list.middleNode());
+    }
+
+    public int middleNode() {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null) {
+            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+        }
+        return slow.data;
+    }
+
+    public void insertAtHead(int data) {
+        if (head == null && tail == null) {
+            Node node = new Node(data);
+            head = node;
+            tail = node;
+        } else {
+            Node node = new Node(data);
+            node.next = head;
+            head = node;
+        }
+    }
+
+    public void insertAtTail(int data) {
+        if (head == null && tail == null) {
+            Node node = new Node(data);
+            head = node;
+            tail = node;
+        } else {
+            Node node = new Node(data);
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+
+}
