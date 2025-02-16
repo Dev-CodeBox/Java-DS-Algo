6ee0394821c405edab06c2b08209432cdd23d1f0
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-02-16 11:15:55 +0000

    Printing -> ( Custom Stack )

--- DS-Algo/Stack/src/CustomStack.java
+++ DS-Algo/Stack/src/CustomStack.java
@@ -21,6 +21,7 @@ public class CustomStack {
         stack.insert(scanner.nextInt());
         stack.delete();
         System.out.println("Size -> " + stack.size());
+        stack.print();
     }
 
     public void insert(int data) {
@@ -48,5 +49,12 @@ public class CustomStack {
             size++;
         }
         return size;
+    }
+
+    public void print() {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
