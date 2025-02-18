66635c61b10789295ca3fd54c09f1dc38ba4d0a5
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-02-18 10:31:19 +0000

    Stack Push

--- DS-Algo/Stack/src/CustomStackUsingNode.java
+++ DS-Algo/Stack/src/CustomStackUsingNode.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CustomStackUsingNode {
     class StackNode {
         int data;
@@ -16,6 +18,23 @@ public class CustomStackUsingNode {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         CustomStackUsingNode st = new CustomStackUsingNode();
+        st.push(scanner.nextInt());
+        st.push(scanner.nextInt());
+        st.push(scanner.nextInt());
+        st.push(scanner.nextInt());
+        st.push(scanner.nextInt());
+    }
+
+    public void push(int data) {
+        if (top == null) {
+            StackNode node = new StackNode(data);
+            top = node;
+        } else {
+            StackNode node = new StackNode(data);
+            node.next = top;
+            top = node;
+        }
     }
 }
