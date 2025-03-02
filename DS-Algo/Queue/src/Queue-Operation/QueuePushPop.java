5e892de8e06c3d5e3a48265cd70ec1f611c479ef
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-03-02 09:33:09 +0000

    Queue Operations

--- DS-Algo/Queue/src/Queue-Operation/QueuePushPop.java
+++ DS-Algo/Queue/src/Queue-Operation/QueuePushPop.java
@@ -3,10 +3,20 @@ import java.util.Queue;
 
 public class QueuePushPop {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5 };
-        Queue<Integer> que = push(arr);
-        System.out.print("Queue Elements -> ");
-        pop(que);
+        // int[] arr = { 1, 2, 3, 4, 5 };
+        // Queue<Integer> que = push(arr);
+        // System.out.print("Queue Elements -> ");
+        // pop(que);
+
+        Queue<Integer> que = new LinkedList<>();
+        // Push
+        enqueue(que, 1);
+        // Pop
+        // dequeue(que);
+        // Front
+        System.out.println("Front Element -> " + front(que));
+        // Search
+        System.out.println("Found -> " + find(que, 0));
     }
 
     public static Queue<Integer> push(int arr[]) {
@@ -22,5 +32,26 @@ public class QueuePushPop {
             System.out.print(q.poll() + " ");
         }
         System.out.println();
+    }
+
+    public static void enqueue(Queue<Integer> q, int x) {
+        q.add(x);
+    }
+
+    public static void dequeue(Queue<Integer> q) {
+        q.poll();
+    }
+
+    public static int front(Queue<Integer> q) {
+        return q.peek();
+    }
+
+    public static String find(Queue<Integer> q, int x) {
+        for (int num : q) {
+            if (num == x) {
+                return "Yes";
+            }
+        }
+        return "No";
     }
 }
