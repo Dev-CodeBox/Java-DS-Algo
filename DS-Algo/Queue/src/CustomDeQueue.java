6e1519ca36eedecd1f65625b51e01be12e625520
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-03-03 11:13:53 +0000

    Inserting At Front

--- DS-Algo/Queue/src/CustomDeQueue.java
+++ DS-Algo/Queue/src/CustomDeQueue.java
@@ -12,5 +12,27 @@ public class CustomDeQueue {
 
     public static void main(String[] args) {
         CustomDeQueue deq = new CustomDeQueue(5);
+        deq.pushFront(10);
+        deq.pushFront(20);
+        deq.pushFront(30);
+        deq.print();
+    }
+
+    public static void pushFront(int data) {
+        if (front == -1 && rear == -1) {
+            front++;
+            arr[++rear] = data;
+        } else if (front == 0) {
+            System.out.println("Front At 0");
+        } else {
+            arr[--front] = data;
+        }
+    }
+
+    public static void print() {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
