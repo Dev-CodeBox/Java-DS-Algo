35f255c4025fd521178b8c06a7c4b3f8e4be7459
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-03-06 11:28:55 +0000

    Insertion In Circular Queue

--- DS-Algo/Queue/src/CustomCircularQueue.java
+++ DS-Algo/Queue/src/CustomCircularQueue.java
@@ -1,16 +1,36 @@
 public class CustomCircularQueue {
     static int[] arr;
     static int size;
-    static int front, end;
+    static int front, rear;
 
     CustomCircularQueue(int size) {
         this.arr = new int[size];
         this.size = size;
         this.front = -1;
-        this.end = -1;
+        this.rear = -1;
     }
 
     public static void main(String[] args) {
         CustomCircularQueue q = new CustomCircularQueue(5);
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        q.push(4);
+        q.push(5);
+    }
+
+    public static void push(int val) {
+        if (front == -1 && rear == -1) {
+            front++;
+            arr[++rear] = val;
+        } else if (rear == arr.length - 1 && front != 0) {
+            rear = 0;
+            arr[rear] = val;
+        } else if ((front == 0 && rear == arr.length - 1) || (front - rear == 1)) {
+            System.out.println("Queue Is Full");
+            return;
+        } else {
+            arr[++rear] = val;
+        }
     }
 }
