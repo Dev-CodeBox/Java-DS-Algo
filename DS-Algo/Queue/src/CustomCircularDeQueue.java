5ce6d1ec35a252aca0efe36762877b064ab2e19f
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-03-07 10:25:58 +0000

    Insertion At Front In Circular DeQue

--- DS-Algo/Queue/src/CustomCircularDeQueue.java
+++ DS-Algo/Queue/src/CustomCircularDeQueue.java
@@ -12,5 +12,24 @@ public class CustomCircularDeQueue {
 
     public static void main(String[] args) {
         CustomCircularDeQueue deq = new CustomCircularDeQueue(5);
+        deq.pushFront(1);
+        deq.pushFront(2);
+        deq.pushFront(3);
+        deq.pushFront(4);
+        deq.pushFront(5);
+    }
+
+    public static void pushFront(int data) {
+        if ((front == 0 && rear == arr.length - 1) || (front == rear + 1)) {
+            System.out.println("Dequeue Is Full");
+        } else if (front == -1 && rear == -1) {
+            front++;
+            arr[++rear] = data;
+        } else if (front == 0) {
+            front = arr.length - 1;
+            arr[front] = data;
+        } else {
+            arr[--front] = data;
+        }
     }
 }
