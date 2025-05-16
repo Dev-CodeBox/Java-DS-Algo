bf19e664dee33ecdd8c437f0d471cfe1129478a3
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-05-16 07:38:32 +0000

    Insert Into Custom Binary Tree

--- DS-Algo/Tree/Binary Tree/src/CustomBinaryTree.java
+++ DS-Algo/Tree/Binary Tree/src/CustomBinaryTree.java
@@ -10,5 +10,20 @@ public class CustomBinaryTree {
 
     public static void main(String[] args) {
         CustomBinaryTree root = new CustomBinaryTree(0);
+        root.insertLeft(2);
+        root.insertRight(3);
+
+        root.left.insertLeft(4);
+        root.left.insertRight(5);
+
+        System.out.println("Tree created!");
+    }
+
+    public void insertLeft(int value) {
+        this.left = new CustomBinaryTree(value);
+    }
+
+    public void insertRight(int value) {
+        this.right = new CustomBinaryTree(value);
     }
 }
