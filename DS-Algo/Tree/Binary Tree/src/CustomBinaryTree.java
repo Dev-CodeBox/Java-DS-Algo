6a1e1a1d406b0a0782b0278b3adf972629479ad3
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2025-05-16 07:41:25 +0000

    Preorder Traversal

--- DS-Algo/Tree/Binary Tree/src/CustomBinaryTree.java
+++ DS-Algo/Tree/Binary Tree/src/CustomBinaryTree.java
@@ -17,6 +17,8 @@ public class CustomBinaryTree {
         root.left.insertRight(5);
 
         System.out.println("Tree created!");
+
+        root.preorder(root);
     }
 
     public void insertLeft(int value) {
@@ -25,5 +27,13 @@ public class CustomBinaryTree {
 
     public void insertRight(int value) {
         this.right = new CustomBinaryTree(value);
+    }
+
+    public static void preorder(CustomBinaryTree node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            preorder(node.left);
+            preorder(node.right);
+        }
     }
 }
